7b1908bb3d63ab341341d9db7c87059955ea1542
Kian Seibel <you@example.com>
2026-05-03 02:03:12 +0400

    add uploadFile function and his routing

--- fileserver.go
+++ fileserver.go
@@ -13,6 +13,9 @@
 //
 //	$ curl http://localhost:3333/files/notes.txt
 //	Notessszzz
+//
+//	$ curl -X POST -F "file=@./01 - Murmaider.mp3" http://localhost:3333/files/upload
+//  {"filename":"01 - Murmaider.mp3","ok":true,"path":"data/01 - Murmaider.mp3","size":8383732}
 package main
 
 import (
@@ -21,6 +24,9 @@ import (
 	"path/filepath"
 	"strings"
 
+	"encoding/json"
+	"io"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -28,11 +34,6 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-
-	// Index handler
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hi"))
-	})
 
 	// Create a route along /files that will serve contents from
 	// the ./data/ folder.
@@ -50,16 +51,64 @@ func FileServer(r chi.Router, path string, root http.FileSystem) {
 		panic("FileServer does not permit any URL parameters.")
 	}
 
+	r.Post(path + "/upload", UploadFile)
+
 	if path != "/" && path[len(path)-1] != '/' {
 		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
 		path += "/"
 	}
 	path += "*"
 
+
 	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
 		rctx := chi.RouteContext(r.Context())
 		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
 		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
 		fs.ServeHTTP(w, r)
+	})
+}
+
+func UploadFile(w http.ResponseWriter, r *http.Request) {
+	const maxUploadSize = 50 << 20 // 50 MB
+
+	err := r.ParseMultipartForm(maxUploadSize)
+	if err != nil {
+		http.Error(w, "file too large or invalid multipart form", http.StatusBadRequest)
+		return
+	}
+
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "missing file field", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	err = os.MkdirAll("./data", 0755)
+	if err != nil {
+		http.Error(w, "cannot create upload dir", http.StatusInternalServerError)
+		return
+	}
+
+	dstPath := filepath.Join("./data", filepath.Base(header.Filename))
+	dst, err := os.Create(dstPath)
+	if err != nil {
+		http.Error(w, "cannot create destination file", http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		http.Error(w, "cannot save file", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]any{
+		"ok":       true,
+		"filename": header.Filename,
+		"size":     header.Size,
+		"path":     dstPath,
 	})
 }
