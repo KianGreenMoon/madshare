ad7d25f36fa14dc75dc93d488103feda99fd011c
Kian Seibel <you@example.com>
2026-05-10 07:52:28 +0400

    some logs

--- madshare.go
+++ madshare.go
@@ -8,16 +8,20 @@ import (
 func main() {
 	log.Println("Start the program")
 	var wg sync.WaitGroup
+	log.Println("Starting api...")
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		route()
 	}()
+	log.Println("Started!")
+	log.Println("Starting web-ui...")
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		mainui()
 	}()
+	log.Println("Started!")
 	wg.Wait()
 	log.Println("End!")
 }
